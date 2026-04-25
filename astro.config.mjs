a6307e85af09487e38d47acee70e5504aa677559
annetteraffan <haveyouseenphilately@gmail.com>
2026-04-25 09:03:50 +0100

    fix image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
 	site: 'https://example.com',
 	image: {
 		service: {entrypoint: 'astro/assets/services/noop'}
-	}
+	},
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
